6b0f4af84d1c2b1b1884540206d8e65ec22b4796
Harold Sorel <hsorel@fu-r3-p6.s19.be>
2025-03-18 18:44:25 +0100

    ok

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,12 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: haroldsorel <marvin@42.fr>                 +#+  +:+       +#+        */
+/*   By: hsorel <hsorel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 16:17:45 by haroldsorel       #+#    #+#             */
-/*   Updated: 2025/02/03 16:17:47 by haroldsorel      ###   ########.fr       */
+/*   Updated: 2025/03/18 18:15:19 by hsorel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "Fixed.hpp"
 
 int main( void )
@@ -18,7 +19,6 @@ int main( void )
     Fixed const c( 42.42f );
     Fixed const d( b );
     a = Fixed( 1234.4321f );
-    
     std::cout << "a is " << a << std::endl;
     std::cout << "b is " << b << std::endl;
     std::cout << "c is " << c << std::endl;
@@ -27,5 +27,33 @@ int main( void )
     std::cout << "b is " << b.toInt() << " as integer" << std::endl;
     std::cout << "c is " << c.toInt() << " as integer" << std::endl;
     std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+}
+
+/*
+int main( void )
+{
+    Fixed a;
+    Fixed const b( 10 );
+    Fixed const c( 42.92f );
+    Fixed const d( b );
+    a = Fixed( 1234.9321876876f );
+
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl << std::endl;
+    std::cout << "raw values : " << a.getRawBits() << std::endl;
+    std::cout << "raw values : " << b.getRawBits() << std::endl;
+    std::cout << "raw values : " << c.getRawBits() << std::endl;
+    std::cout << "raw values : " << d.getRawBits() << std::endl << std::endl;
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+    std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+    std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+    std::cout << "d is " << d.toFloat() << " as float" << std::endl;
     return 0;
 }
+*/
